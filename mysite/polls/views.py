c39aa03ff775beb461731698587b15b10d921838
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-09-23 13:00:23 -0500

    django

---
+++ mysite/polls/views.py
@@ -0,0 +1,20 @@
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+def index(request):
+    return HttpResponse("This is a bad request, Start with the music route.")
+
+def function(request):
+    return HttpResponse("My favorite music artist is Beyonce.")
+
+def function(request):
+    return HttpResponse("Beyonce is the number one artist.")
+
+def function(request):
+    return HttpResponse("Blue Ivy is one of my favorite music artist..")
+
+
+
